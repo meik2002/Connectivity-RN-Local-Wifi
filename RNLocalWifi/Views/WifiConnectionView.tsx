2300cb787ab3adc67f3a573be7e293a5269031ba
Min Kim <minsik.kim@generac.com>
2023-11-28 13:39:02 -0600

    it works

--- RNLocalWifi/Views/WifiConnectionView.tsx
+++ RNLocalWifi/Views/WifiConnectionView.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import {Button, View} from 'react-native/types';
-import {connectToSsidWithPrefix} from '../Features/WifiSlice';
-import {useAppDispatch} from '../Store/Store';
+import React, {useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
+
+import {connectToSsidWithPrefix, currentSsid} from '../Features/WifiSlice';
+import {useAppDispatch, useAppSelector} from '../Store/Store';
 
 function WifiConnectionView() {
   const dispatch = useAppDispatch();
+  const [ssid, setSsid] = useState('');
+
+  const connectToMlgSsid = (prefix: string) => {
+    WifiManager.connectToSSIDPrefix(prefix).catch(e => {
+      console.error('ConnectToSsidWithPrefix: ', e);
+      setSsid(`connectToMlgSsid - [${prefix}]\n` + JSON.stringify(e));
+    });
+  };
+
+  const checkSsid = () => {
+    console.log('check ssid');
+    WifiManager.getCurrentWifiSSID()
+      .then(result => {
+        console.log(result);
+        setSsid(result);
+      })
+      .catch(e => {
+        console.log('Error while detecting: ', e);
+        setSsid('checkSsid\n' + JSON.stringify(e));
+      });
+  };
 
   return (
     <View>
       <Button
-        onPress={() => dispatch(connectToSsidWithPrefix('MGL'))}
-        title="HELP!"
+        onPress={() => connectToMlgSsid('BriesWorld')}
+        title="Connect MLG Ssid!"
       />
+      {/* <Text>{ssid}</Text> */}
+
+      {/* <Button onPress={() => dispatch(currentSsid())} title="Current SSID ??" /> */}
+
+      <Button onPress={checkSsid} title="Check Current SSID ??" />
+
+      <Text>{ssid}</Text>
     </View>
   );
 }
